74578b5b79560576bd39157a6d800feb43f19992
jhson <jhson@encar.com>
2017-01-23 16:16:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const config = require('./config')
+const port = process.env.PORT || 7777
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use(morgan('dev'))
+
+app.set('jwt-secret', config.secret)
+
+app.get('/', (req, res) => {
+    res.send('Hello JWT')
+})
+
+app.listen(port, () => {
+    console.log(`Express is running on port ${port}`)
+})
+
+mongoose.connect(config.mongodbUri)
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', () => {
+    console.log('connected to mongodb server')
+})
